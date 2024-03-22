19b70db3846138414bc047d0cf48e81eb2216193
unknown <unknown@example.com>
2024-03-22 20:01:40 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,74 @@
+// index.js
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {
+      avatarUrl: defaultAvatarUrl,
+      nickName: '',
+      src: '',
+    },
+    takePhoto() {  
+      const ctx = wx.createCameraContext()  
+      ctx.takePhoto({  
+        quality: 'high',  
+        success: (res) => {  
+          this.setData({  
+            src: res.tempImagePath  
+          })  
+          // 这里可以调用后端识别接口  
+          this.recognizePerson(res.tempImagePath)  
+        }  
+      })  
+    },
+    recognizePerson(imagePath) {  
+      // 调用后端识别接口，传递图片路径或Base64编码等  
+      // ...  
+    },    
+    hasUserInfo: false,
+    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+  },
+  onShareAppMessage() {
+    return {
+    title: '地大喵汪星球',
+    desc: '一个专门分享地大小猫小狗的优质小程序',
+    path: '/pages/index'
+    }
+},
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onChooseAvatar(e) {
+    const { avatarUrl } = e.detail
+    const { nickName } = this.data.userInfo
+    this.setData({
+      "userInfo.avatarUrl": avatarUrl,
+      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+    })
+  },
+  onInputChange(e) {
+    const nickName = e.detail.value
+    const { avatarUrl } = this.data.userInfo
+    this.setData({
+      "userInfo.nickName": nickName,
+      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+    })
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+})
